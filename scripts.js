8042612d9f5935a22bbbe5b94b28264bb2bbbfb4
pennyflipper.dev <76460018+SaanSaelee@users.noreply.github.com>
2022-08-22 22:34:37 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let addOne = document.getElementById("add-el");
+let subtractOne = document.getElementById("subtract-el");
+
+let counter = document.getElementById("counter");
+
+console.log(counter);
+
+let count = 0;
+
+function add() {
+  counter.textContent = count;
+}
+
+add();
