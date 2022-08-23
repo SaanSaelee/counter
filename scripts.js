5941bd82e53f379216de4c17dd6113b9e51766bd
pennyflipper.dev <76460018+SaanSaelee@users.noreply.github.com>
2022-08-23 00:29:24 -0700

    added people images

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-let addOne = document.getElementById("add-el");
-let subtractOne = document.getElementById("subtract-el");
-
-let counter = document.getElementById("counter");
-
-console.log(counter);
+let addBtn = document.getElementById("add-btn");
+let subtractBtn = document.getElementById("subtract-btn");
+let counter = document.getElementById("attendees");
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
 function add() {
+  count += 1;
   counter.textContent = count;
 }
 
-add();
+function subtract() {
+  count -= 1;
+  counter.textContent = count;
+  if (count === 0) {
+    counter.textContent = "All attendees have left.";
+    alert(
+      "The event has ended. You don't have to go home, but you can't stay here."
+    );
+  }
+}
+
+function save() {
+  let lastCount = count + ", ";
+  saveEl.textContent += lastCount;
+}
